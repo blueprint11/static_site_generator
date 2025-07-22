ee437bc1052cdfcd1a050b00a072783dc8d35a41
blueprint11 <goyalakshay02gmail.com>
2025-07-22 12:46:30 +0000

    converting inline node to a html node

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 class Inline(Enum):
     TEXT="text"
     BOLD="bold"
@@ -19,3 +20,18 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text},{self.text_type.value},{self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type.value=="text":
+        return LeafNode(None,text_node.text,None)
+    if text_node.text_type.value=="bold":
+        return LeafNode("b",text_node.text,None)
+    if text_node.text_type.value=="italic":
+        return LeafNode("i",text_node.text,None)
+    if text_node.text_type.value=="code":
+        return LeafNode("code",text_node.text,None)
+    if text_node.text_type.value=="link":
+        return LeafNode("a",text_node.text,{"href": text_node.url})
+    if text_node.text_type.value=="image":
+        return LeafNode("img","",{"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
