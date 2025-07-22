16edad2218b68856f8f7ebcefd07559157db9a48
blueprint11 <goyalakshay02gmail.com>
2025-07-22 07:41:30 +0000

    HTMLNode class wit __repr__ and other attributes to go into tree

---
+++ src/htmlnode.py
@@ -0,0 +1,19 @@
+
+
+class HTMLNode:
+    def __init__(self,tag=None,value=None,children=None,props=None):
+        self.tag=tag                            #like p,h1 
+        self.value=value                        #string repr.of value of tag like paragraph
+        self.children=children                  #list of html node objects tht are children to node
+        self.props=props                        #dict of key value pairs for attributes of html tag like <a>href
+    def to_html(self):
+        raise NotImplementedError
+    def props_to_html(self):
+        s=""
+        if self.props is None:
+            return ""
+        for key,value in self.props.items():
+            s+= f' {key}="{value}"'
+        return s
+    def __repr__(self):
+        return f"HTMLNode({self.tag},{self.value},children:{self.children},{self.props})"
