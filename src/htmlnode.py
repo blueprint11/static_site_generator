80e45f00d1f1d1e7ba6bcc92c76bce9f3138899f
blueprint11 <goyalakshay02gmail.com>
2025-07-22 12:09:23 +0000

    added Parent node class , children mandatory ,value is not there, props could be

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,19 @@ class LeafNode(HTMLNode):
         
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag==None:
+            raise ValueError("invalid Html: no tag")
+        if self.children==None:
+            raise ValueError("invalid parent:no children")
+        else:
+            s=""
+            for children in self.children:
+                s+=children.to_html()
+            return f"<{self.tag}{self.props_to_html()}>{s}</{self.tag}>"
+            
